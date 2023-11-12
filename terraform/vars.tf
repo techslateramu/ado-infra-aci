b9dd89b931cda90f79cb8f8707eb972836d81a11
Ramu Gurrapu <ramu@techslatelimited.com>
2023-11-12 11:51:50 +0000

    fixing variables with calc image

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -12,6 +12,11 @@ variable "ARM_CLIENT_ID"        {}
 variable "ARM_CLIENT_SECRET"    {}
 
 variable "specific_tags"        { default = {} }
-variable "main_project"         { default = "calc" }
+
+variable "main_project"         { default = "" }   # Set this VARIABLE - this will form  the name of RESOURCES
+
 variable "sub_project"          { default = "01" }
-variable "location"             { default = "uksouth" } # https://github.com/claranet/terraform-azurerm-regions/blob/master/regions.tf
+variable "location"             { default = "uksouth" }                             # https://github.com/claranet/terraform-azurerm-regions/blob/master/regions.tf
+variable "container_image"      { default = "linuxramu/calc:2.5" }
+
+variable "dns_name_label"       { default = "" }  # Set this VARIABLE - this will be URL of application, should be unique
